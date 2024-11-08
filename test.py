46c9a6adcb3a10c40f8ddfb8072b9d4429430d0f
Hasan <73844788+hasan221b@users.noreply.github.com>
2024-11-08 21:47:23 +0100

    Update test.py

--- test.py
+++ test.py
@@ -2,13 +2,16 @@ import requests
 
 
 url = "http://127.0.0.1:8000/price"
-
+cart = int(input("Enter Diamond Cart"))
+x = int(input("Enter Diamond x"))
+y = int(input("Enter Diamond y"))
+z = int(input("Enter Diamond z"))
 #data that will be passed to the api
 testing_data = {
-    "cart": 5,
-    "x": 10,
-    "y": 2,
-    "z": 4 
+    "cart": cart,
+    "x": x,
+    "y": y,
+    "z": z 
     }
 
 response = requests.post(url, json=testing_data)
